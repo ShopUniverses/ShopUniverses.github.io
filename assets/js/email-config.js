108769c9f432451c536f2c5d5fa7ca863f9220e6
Jdparra2004 <jdparra2004@gmail.com>
2026-01-13 16:34:51 -0500

    Proyecto base reciclado

---
+++ assets/js/email-config.js
@@ -0,0 +1,11 @@
+/*
+    EMAIL-CONFIG.JS — Inicialización de EmailJS
+    Autor: Juan David Parra Cantor
+    Descripción:
+        Este archivo se encarga únicamente de inicializar
+        el servicio EmailJS con la Public Key del proyecto.
+*/
+
+(function () {
+    emailjs.init("gj8TMdXOaWDqBYHnp");
+})();
